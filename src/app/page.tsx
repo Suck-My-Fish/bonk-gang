19dfde2c14030d47e18c5fa2e7f89d983a14dfe9
srbpgh <srbpgh@gmail.com>
2024-03-24 19:44:50 -0400

    setup website for react next development. see readme for how to run

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main>
+      <header>
+        <h1>Hello Bonk Gangers!</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Suck My Fish</a></li>
+                <li><a href="#">Sandyman31</a></li>
+                <li><a href="#">Dr_ Tree</a></li>
+            </ul>
+        </nav>
+      </header>  
+    </main>
+  );
+}
